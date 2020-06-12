b639b1778080236da0ef6934a4f2895fa6d95bd0
ZoeDj <studio3fun@gmail.com>
2020-06-12 19:49:07 -0400

    adding a new user

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import UserTable from "./tables/UserTable";
+import AddUserForm from "./AddUserForm";
 
 const App = () => {
   const usersData = [
@@ -10,12 +11,18 @@ const App = () => {
 
   const [users, setUsers] = useState(usersData);
 
+  const addUser = (user) => {
+    user.id = user.length + 1;
+    setUsers([...users, user]);
+  };
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div class="flex-row">
         <div>
           <h3>Add User</h3>
+          <AddUserForm addUser={addUser} />
         </div>
         <div>
           <h3>View Users</h3>
